2e09e1b3d6259b3dbbd8dc555cebb885d3178d0a
Nicole Pâmela <138784922+nicolepamelaa@users.noreply.github.com>
2023-12-06 18:34:36 -0300

    Connect Idoso

--- usuarioPrestador/fotosProfissional.js
+++ usuarioPrestador/fotosProfissional.js
@@ -1,13 +1,68 @@
 // CHAMADA MODAL
 
-var modalFotos = document.querySelector('.imgListFotos')
-var upFoto = document.querySelector('.upFoto')
-modalFotos.addEventListener('click', ()=> {
-    upFoto.showModal()
+// MODAL 1
+var modalFotos1 = document.querySelector('.imgListFotos1')
+var upFoto1 = document.querySelector('.upFoto1')
+modalFotos1.addEventListener('click', ()=> {
+    upFoto1.showModal()
 })
 
-var closedModal = document.querySelector('.closedModal')
+// MODAL 2
+var modalFotos2 = document.querySelector('.imgListFotos2')
+var upFoto2 = document.querySelector('.upFoto2')
+modalFotos2.addEventListener('click', ()=> {
+    upFoto2.showModal()
+})
+
+// MODAL 3
+var modalFotos3 = document.querySelector('.imgListFotos3')
+var upFoto3 = document.querySelector('.upFoto3')
+modalFotos3.addEventListener('click', ()=> {
+    upFoto3.showModal()
+})
+
+// MODAL 4
+var modalFotos4 = document.querySelector('.imgListFotos4')
+var upFoto4 = document.querySelector('.upFoto4')
+modalFotos4.addEventListener('click', ()=> {
+    upFoto4.showModal()
+})
+
+// MODAL 5
+var modalFotos5 = document.querySelector('.imgListFotos5')
+var upFoto5 = document.querySelector('.upFoto5')
+modalFotos5.addEventListener('click', ()=> {
+    upFoto5.showModal()
+})
+
+
+
+
+// CLOSE MODAL
+
+var closedModal = document.querySelector('.closedModal1')
 closedModal.addEventListener('click', () =>{
-    upFoto.close()
+    upFoto1.close()
     })
+
+    var closedModal = document.querySelector('.closedModal2')
+closedModal.addEventListener('click', () =>{
+    upFoto2.close()
+    })
+
+    var closedModal = document.querySelector('.closedModal3')
+closedModal.addEventListener('click', () =>{
+    upFoto3.close()
+    })
+
+    var closedModal = document.querySelector('.closedModal4')
+closedModal.addEventListener('click', () =>{
+    upFoto4.close()
+    })
+
+    var closedModal = document.querySelector('.closedModal5')
+closedModal.addEventListener('click', () =>{
+    upFoto5.close()
+    })
+
     
